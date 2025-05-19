d2cb43031c1ecbd6d40edf9d972f406bec977d1b
BIBIN THANKACHAN <bibinthankachan29@gmail.comgit config --global user.name>
2025-05-19 13:46:13 +0530

    doctors

--- Client/src/pages/Doctors.jsx
+++ Client/src/pages/Doctors.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext} from '../context/AppContext'
 
 const Doctors = () => {
+  const {speciality} = useParams()
+  const {filterDoc,setFilterDoc} = useState([])
+  const {doctors} = useContext(AppContext)
   return (
     <div>
-      
+      <p>Browse through the doctors specialist.</p>
+      <div>
+        <div>
+          <p>General physician</p>
+          <p>Gynecologist</p>
+          <p>Dermatologist</p>
+          <p>Pediatrician</p>
+          <p>Neurologist</p>
+          <p>Gastroenterologist</p>
+        </div>
+        <div>
+{
+  filterDoc.map()
+}
+        </div>
+      </div>
     </div>
   )
 }
