fd0656f749be08c8c232cbda33ec8c65760ec388
BIBIN THANKACHAN <bibinthankachan29@gmail.comgit config --global user.name>
2025-05-27 12:09:05 +0530

    mob v

--- FRONT-END/src/pages/Doctors.jsx
+++ FRONT-END/src/pages/Doctors.jsx
@@ -27,7 +27,7 @@ const Doctors = () => {
       <div className="flex flex-col sm:flex-row items-start gap-5 mt-5">
         <button className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showfilter ? 'bg-blue-500 text-white' : ''} `} onClick={() => setShowFilter(prev => !prev)}>filters</button>
         {/* Speciality list */}
-        <div className={`flex-col gap-2 bg-blue-50 rounded-lg p-4 min-w-[180px] ${showfilter ? 'flex' : 'hidden'} sm:flex`}>
+        <div className={`flex flex-col gap-4 text-sm text-gray-600 ${showfilter ? 'flex' : 'hidden sm:flex'}`}>
           <p onClick={() => speciality === 'General physician' ? navigate('/doctors') : navigate('/doctors/General physician')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'General physician' ? 'bg-indigo-100 text-black' : ''}`}>General physician</p>
           <p onClick={() => speciality === 'Gynecologist' ? navigate('/doctors') : navigate('/doctors/Gynecologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Gynecologist' ? 'bg-indigo-100 text-black' : ''}`}>Gynecologist</p>
           <p onClick={() => speciality === 'Dermatologist' ? navigate('/doctors') : navigate('/doctors/Dermatologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Dermatologist' ? 'bg-indigo-100 text-black' : ''}`}>Dermatologist</p>
